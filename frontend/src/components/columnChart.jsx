0e159ca68b1a174a4920d0d267678cc173881358
Daniel-jpg1 <carlosdanield.alves@gmail.com>
2026-03-26 16:48:37 -0300

    feat: start monthly column chart structure

--- frontend/src/components/columnChart.jsx
+++ frontend/src/components/columnChart.jsx
@@ -2,6 +2,28 @@ import { useState, useEffect } from "react";
 
 function ColumnChart() {
   const [debt, setDebt] = useState([]);
+  const maxValue = 10000;
+  let leftSteps = 5;
+  let labels = [];
+  const months = useState([
+    { month: "Jan", recebidas: 500, feitas: 400, dividas: 300 },
+    { month: "Fev", recebidas: 500, feitas: 400, dividas: 300 },
+    { month: "Mar", recebidas: 500, feitas: 400, dividas: 300 },
+    { month: "Abr", recebidas: 500, feitas: 400, dividas: 300 },
+    { month: "Mai", recebidas: 500, feitas: 400, dividas: 300 },
+    { month: "Jun", recebidas: 500, feitas: 400, dividas: 300 },
+    { month: "Jul", recebidas: 500, feitas: 400, dividas: 300 },
+    { month: "Ago", recebidas: 500, feitas: 400, dividas: 300 },
+    { month: "Set", recebidas: 500, feitas: 400, dividas: 300 },
+    { month: "Out", recebidas: 500, feitas: 400, dividas: 300 },
+    { month: "Nov", recebidas: 500, feitas: 400, dividas: 300 },
+    { month: "Dez", recebidas: 500, feitas: 400, dividas: 300 },
+  ]);
+  console.log(months);
+
+  for (let i = 0; i <= leftSteps; i++) {
+    labels.push(Math.round((maxValue / leftSteps) * i));
+  }
 
   useEffect(() => {
     async function fetchDebts() {
@@ -24,7 +46,6 @@ function ColumnChart() {
         }
 
         setDebt(data);
-        console.log(data);
       } catch (error) {
         console.error("Erro ao mostrar dados", error);
       }
@@ -38,6 +59,13 @@ function ColumnChart() {
       {debt.map((item) => {
         return <h1 key={item.id}>{item.total_amount}</h1>;
       })}
+      {months.map((month) => {
+        return (
+          <h1 key={month.month}>
+            {month.month} {month.recebidas} {month.feitas} {month.dividas}
+          </h1>
+        );
+      })}{" "}
     </section>
   );
 }
